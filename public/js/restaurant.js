db85a82403b6a4594f06d88c7d9a5f1d1e7d6427
Joshua Lo <josh.lol1995@gmail.com>
2020-03-20 10:46:26 -0700

    committing small changes before pulling from master

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -1,34 +1,37 @@
-const express = require('express');
-var searchQuery = "https://api.yelp.com/v3/businesses/search";
+var searchQuery = "https://api.yelp.com/v3/businesses/search"; // Make a GET request to this
 
 // Parameters
-// Term (string) (search term) -- Required
+// TODO Get values from text boxes? How do we cleanly acquire all of these from the user?
 
-// Location (string) -- REQUIRED
-// Need to provide as lat/long (decimal), so give user option to search for restaurants within a certain city?
+// TODO Term (string) (search term) -- REQUIRED
+// just search "restaurant"
+searchQuery += "?term=restaurant";
 
-// Radius (int) -- Optional
+// TODO Location (string) -- REQUIRED
+// TODO Need to provide as lat/long (decimal)
+// so give user option to search for restaurants within a certain city?
+
+// TODO Radius (int) -- Optional
 // Suggested search radius in meters, probably convert kms to freedom units
 
-// Categories -- Optional
+// TODO Categories -- Optional
 // Do we want users to be able to narrow it down to a type of food then search restaurants within that category?
 
-// limit and offset (int) -- REQUIRED
+// TODO limit and offset (int) -- REQUIRED
 // limit is the # of businesses to return, offset is the list of returned business results by this amount
 // e.g. Page 1 - limit 50, offset 0
 //      Page 2 - limit 50, offset 50
 //      Page 3 - limit 50, offset 100 and etc.
+searchQuery += "/" + 50
 
-// price (string) -- Optional
+// TODO price (string) -- Optional
 // Type 1,2,3 to search for tiers 1, 2, and 3. Goes from 1 to 4 dollar signs.
 
-// open_now (boolean) -- Optional
+// TODO open_now (boolean) -- Optional
 // Returns only open restaurants
 
-// open_at (int) -- Optional
-
+// TODO open_at (int) -- Optional
 // If specified, will return businesses open at the given time (must present as unix time)
 
-// If specified, will return businesses open at the given time (must present as unix time)
-
+// TODO Make the GET request
 
