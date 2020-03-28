c8eb6a655f0e61c0a4ff6aa5b82f7802035a44f7
Caleb Brochu <caleb.brochu@gmail.com>
2020-03-27 21:59:17 -0700

    Adding match functionality

--- public/js/userTwoRestaurants.js
+++ public/js/userTwoRestaurants.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
     var index = 0;
 
     $.get(`/api/showLikes/${userOne}`).then(function(data) {
-        localStorage.clear()
-
+        
         //setting initial restaurant to the first restaurant in the array
         localStorage.setItem('data', JSON.stringify(data));
         
@@ -19,12 +18,19 @@ $(document).ready(function(){
 
     //displaying next restaurant when user clicks thumbs up
     $("#thumbs-up").on("click", function() {
-        console.log("like");
+        //console.log("like");
+        var match = {
+            name: data[index].restaurantId,
+            image: data[index].imageURL
+        };
+        localStorage.setItem('match', JSON.stringify(match));
+        console.log(match);
     });
 
     $("#thumbs-down").on("click", function() {
         nextRestaurant();
-        console.log("not-like");
+        //console.log("not-like");
+        index += 1;
     });
 
     function nextRestaurant() {
@@ -32,10 +38,19 @@ $(document).ready(function(){
         if(index < data.length) {
             $("#restaurant-name").text(data[index].restaurantId);
             $("#restaurant-image").attr("src", data[index].imageURL);
-            index += 1;
+            
         }
         else {
             //$("#see-matches").show();
+            
         };
     };
+
+    let match = JSON.parse(localStorage.getItem('match'));
+    console.log(match)
+        $("#match-name").text(match.name);
+        $("#match-image").attr("src", match.image);
+
+
+
 });
