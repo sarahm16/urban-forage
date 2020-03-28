77d9bf4a6d515689ed461de5035c0582bf1069d5
Joshua Lo <josh.lol1995@gmail.com>
2020-03-28 04:38:06 -0700

    cleaned some comments up

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,7 +1,7 @@
 const yelp = require('yelp-fusion');
 const client = yelp.client('CfJsK0imSbCC04VXKuXZP2fd6UwnnmroGUxGl3xKIimg8ytZBYh3AdZDkPa0JygWzXv2NuutlXHXc8G7thannqJI-PPKEYOHynD_BR8_082Hyx6e_AIQtgvWmCN8XnYx');
 
-async function main(cityName) {
+function main(cityName) {
     getRestaurantData(cityName);
 };
 
